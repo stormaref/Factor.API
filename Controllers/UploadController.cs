9acd3c6ef4938c50f661ab418d532944cdc38bb4
Aref Azizian <storm.aref@outlook.com>
2020-05-16 13:57:33 +0430

    Add project files.

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Factor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile([FromForm(Name = "file")]IFormFile file)
+        {
+            if (file != null && file.ContentType == "image/jpeg")
+            {
+                var bytes = new byte[file.Length];
+                file.OpenReadStream().Read(bytes, 0, bytes.Length);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
